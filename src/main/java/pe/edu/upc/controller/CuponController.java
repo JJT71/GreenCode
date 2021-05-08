1fb34da2947dd3c570041f4c85652ee60e5d1098
Brignith Izumi Gomez Vargas <brignith.2@gmail.com>
2021-05-08 00:18:18 -0500

    front Recolector-Pedido

--- src/main/java/pe/edu/upc/controller/CuponController.java
+++ src/main/java/pe/edu/upc/controller/CuponController.java
@@ -20,7 +20,7 @@ import pe.edu.upc.service.IRecicladorService;
 public class CuponController implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+ //hola
 	@Inject
 	private ICuponService cService;
 	@Inject
